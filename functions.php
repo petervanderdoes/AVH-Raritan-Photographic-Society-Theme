9c62b1a0dc39d70722c46f228b23eacb3ab3f5c4
Peter van der Does <github@avirtualhome.com>
2012-07-31 15:33:50 -0400

    Front-end profile editor

--- functions.php
+++ functions.php
@@ -157,12 +157,10 @@ function my_em_styles_event_output_condition( $replacement, $condition, $match, 
 
 /**
  * Used by page-edit-profile template
- *
  */
 
-define( 'RPS_GF_PROFILE', 2 );	// define the ID number of your profile form.
-
-// ============================================================= PROFILE EDITING
+define( 'RPS_GF_PROFILE', 2 ); // define the ID number of your profile form.
+// PREPOPULATE FORM FIELDS //
 
 /**
  * These are the user metadata fields, with their names and the data about them.
@@ -170,29 +168,69 @@ define( 'RPS_GF_PROFILE', 2 );	// define the ID number of your profile form.
  * in the public profile, the format, etc. In this example, we just have the
  * index of each item so we can reference it easily.
  */
-function rps_gf_profile_metafields()
+add_filter('gform_pre_render_' . RPS_GF_PROFILE, 'rps_populate_profile_fields');
+
+function rps_populate_profile_fields ($form)
 {
-	return array(
-		'displaynamepref' => 	array( 'gf_index' => 2,	),
-		'age' => 		array( 'gf_index' => 3,	),
-		'sex' =>		array( 'gf_index' => 4,	),
-		'location' =>		array( 'gf_index' => 5,	),
-		'twitter' =>		array( 'gf_index' => 6,	),
-	);
+	$profileuser = wp_get_current_user();
+	$rps_gf_meta = array ( 'first_name' => 'first_name', 'last_name' => 'last_name', 'nickname' => 'nickname', 'email' => 'user_email', 'website' => 'user_url' );
+
+	foreach ($form['fields'] as &$field) {
+
+		foreach ($rps_gf_meta as $gf_key => $meta_key) {
+			if (strpos($field['cssClass'], 'rps-profile-' . $gf_key) !== false) {
+				$field['defaultValue'] = $profileuser->$meta_key;
+			}
+		}
+		if (strpos($field['cssClass'], 'rps-profile-name') !== false) {
+			$gf_name_id = $field['id'];
+			$field['defaultValue'][$gf_name_id . '.3'] = $profileuser->first_name;
+			$field['defaultValue'][$gf_name_id . '.6'] = $profileuser->last_name;
+		}
+		if (strpos($field['cssClass'], 'rps-profile-display-name') !== false) {
+			$public_display = array ();
+			$public_display['display_nickname'] = $profileuser->nickname;
+			$public_display['display_username'] = $profileuser->user_login;
+
+			if (! empty($profileuser->first_name))
+				$public_display['display_firstname'] = $profileuser->first_name;
+
+			if (! empty($profileuser->last_name))
+				$public_display['display_lastname'] = $profileuser->last_name;
+
+			if (! empty($profileuser->first_name) && ! empty($profileuser->last_name)) {
+				$public_display['display_firstlast'] = $profileuser->first_name . ' ' . $profileuser->last_name;
+				$public_display['display_lastfirst'] = $profileuser->last_name . ' ' . $profileuser->first_name;
+			}
+
+			if (! in_array($profileuser->display_name, $public_display)) // Only add this if it isn't duplicated elsewhere
+				$public_display = array ( 'display_displayname' => $profileuser->display_name ) + $public_display;
+
+			$public_display = array_map('trim', $public_display);
+			$public_display = array_unique($public_display);
+			foreach ($public_display as $id => $item) {
+				$isSelected = ($profileuser->display_name == $item ? 1 : null);
+				$choices[] = array ( 'text' => $item, 'value' => $item, 'isSelected' => $isSelected );
+			}
+			$field['choices'] = $choices;
+		}
+	}
+
+	return $form;
 }
 
 /**
  * Update the user's profile with information from the received profile GF.
  * run last - just to make sure that everything is fine and dandy.
  */
-function rps_gf_profile_update( $entry, $form )
+function rps_gf_profile_update ($entry, $form)
 {
 	// make sure that the user is logged in
-	// we shouldn't get here because the form should check for logged in users...
-	if ( !is_user_logged_in() )
-	{
-		wp_redirect( home_url() );
-		exit;
+	// we shouldn't get here because the form should check for logged in
+	// users...
+	if (! is_user_logged_in()) {
+		wp_redirect(home_url());
+		exit();
 	}
 
 	// get current user info...
@@ -200,45 +238,21 @@ function rps_gf_profile_update( $entry, $form )
 	get_currentuserinfo();
 
 	// do the user data fields...
-	$new_user_data = array(
-		'ID' => $current_user->ID,
-		'first_name' => $entry['1.3'],	// these are the ID numbers of these fields in our GF
-		'last_name' => $entry['1.6'],
-	);
+	$new_user_data = array ( 'first_name' => $entry['1.3'], 	// these are the ID numbers of these fields in our GF
+	'last_name' => $entry['1.6'] );
 
 	// build the metadata from the entry
-	$new_user_metadata = array();
-	foreach ( rps_gf_profile_metafields() as $field_name => $info )
-	{
-		$new_user_metadata[ $field_name ] = $entry[ $info['gf_index'] ];
+	$new_user_metadata = array ();
+	$gf_fields['first_name'] = array ( 'gf_index' => '1.3', 'wp_meta' => 'first_name' );
+	$gf_fields['last_name'] = array ( 'gf_index' => '1.6', 'wp_meta' => 'last_name' );
+	$gf_fields['nickname'] = array ( 'gf_index' => 2, 'wp_meta' => 'nickname' );
+	$gf_fields['nickname'] = array ( 'gf_index' => 2, 'wp_meta' => 'nickname' );
+	$gf_fields['display_name'] = array ( 'gf_index' => 3, 'wp_meta' => 'display_name' );
+	$gf_fields['email'] = array ( 'gf_index' => 7, 'wp_meta' => 'user_email' );
+	$gf_fields['website'] = array ( 'gf_index' => 8, 'wp_meta' => 'user_url' );
+
+	foreach ($gf_fields as $field_name => $info) {
+		update_user_meta($current_user->ID, $info['wp_meta'], $entry[$info['gf_index']]);
 	}
-
-	// build the display name - (there's almost certainly something in WP to do this already, probably in an admin file)
-	switch ( $new_user_metadata['displaynamepref'] )
-	{
-		// like James
-		case 'first':
-			$display_name = $new_user_data['first_name'];
-			break;
-		// like James C
-		case 'short first':
-			$display_name = $new_user_data['first_name'] . ' ' . ucfirst( substr( $new_user_data['last_name'], 0, 1 ));
-			break;
-		// like J Cooke
-		case 'short last':
-			$display_name = ucfirst( substr( $new_user_data['first_name'], 0, 1 )) . ' ' . $new_user_data['last_name'];
-			break;
-		// like James Cooke.
-		case 'full':
-		default:
-			$display_name = $new_user_data['first_name'] . ' ' . $new_user_data['last_name'];
-			break;
-	}
-	$new_user_data['display_name'] = $display_name;
-
-	// ----------------------------------------------- SAVE ALL THE THINGS
-
-	wp_update_user( $new_user_data );
-	update_user_meta( $current_user->ID, 'rps_profile', $new_user_metadata );
 }
-add_action( 'gform_after_submission_' . RPS_GF_PROFILE, 'rps_gf_profile_update', 100, 2 );
+add_action('gform_after_submission_' . RPS_GF_PROFILE, 'rps_gf_profile_update', 100, 2);
