a53008551832e7b17f503c86535a83ade6b163d6
Peter van der Does <github@avirtualhome.com>
2013-04-24 21:09:45 -0400

    Create function for members only display

--- functions_s2member.php
+++ functions_s2member.php
@@ -21,18 +21,24 @@ add_filter('ws_plugin__s2member_sc_if_conditionals', 'rps_s2member_sc_if_conditi
 function rps_s2member_sc_if_conditionals ($content, $vars)
 {
 	if ( isset($vars['condition_failed']) && $vars['condition_failed'] === TRUE ) {
-		$content = "<fieldset>";
-		$content .= "<legend>";
-		$content .= "<small>Members only</small>";
-		$content .= "</legend>";
-		$content .= "<strong>";
-		if ( is_user_logged_in() ) {
-			$content .= "Only members can see this content.";
-		} else {
-			$content .= "You must be logged to see this content.";
-		}
-		$content .= "</strong>";
-		$content .= "</fieldset>";
+		$content = rps_s2member_display_restriction("Only members can see this content");
 	}
+	return $content;
+}
+
+function rps_s2member_display_restriction ($logged_in_message = "Only members can see this content", $not_logged_in_message = "You must be logged to see this content.")
+{
+	$content = "<fieldset>";
+	$content .= "<legend>";
+	$content .= "<small>Members only</small>";
+	$content .= "</legend>";
+	$content .= "<strong>";
+	if ( is_user_logged_in() ) {
+		$content .= $logged_in_message;
+	} else {
+		$content .= $not_logged_in_message;
+	}
+	$content .= "</strong>";
+	$content .= "</fieldset>";
 	return $content;
 }
