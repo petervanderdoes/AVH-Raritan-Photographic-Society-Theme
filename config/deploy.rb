7db5aa920192665b13ce05fe1848a6a85cd8805c
Peter van der Does <github@avirtualhome.com>
2013-06-15 16:17:39 -0400

    Forgot something

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,8 @@
 set :application, "rps_theme"
 set :repository,  "git@bitbucket.org:petervanderdoes/rps-theme.git"
 set :scm, :git
+_cset(:apc_webroot) { "" }
+
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
