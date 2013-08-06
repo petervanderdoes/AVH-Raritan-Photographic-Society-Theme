7bef96a1d882b699cd83ded631ff8b123f1bdeed
Peter van der Does <github@avirtualhome.com>
2013-08-05 22:26:17 -0400

    Update of the social-icons

--- custom/site-footer.php
+++ custom/site-footer.php
@@ -30,10 +30,10 @@ if ($suf_footer_layout_style != 'in-align') {
 						<?php
 						echo 'Raritan Photographic Society on the web';
 						echo "<ul>";
-echo "<li><a href='https://www.facebook.com/pages/Raritan-Photographic-Society/157106827664588?sk=info'><span class='social-icon social-facebook'></span><span class='social-text'>Facebook</span></a></li>";
-echo "<li><a href='http://www.meetup.com/http-www-raritanphoto-com/'><span class='social-icon social-meetup'></span><span class='social-text'>Meetup</span></a></li>";
-echo "<li><a href='https://plus.google.com/114179804721869477091/about'><span class='social-icon social-google-plus'></span><span class='social-text'>Google+</span></a></li>";
-echo "<li><a href='https://twitter.com/raritanphoto'><span class='social-icon social-twitter'></span><span class='social-text'>Twitter</span></a></li>";
+echo "<li><a href='https://www.facebook.com/pages/Raritan-Photographic-Society/157106827664588?sk=info'><span class='social-icon-16 social-icon-16-facebook'></span><span class='social-text'>Facebook</span></a></li>";
+echo "<li><a href='http://www.meetup.com/http-www-raritanphoto-com/'><span class='social-icon-16 social-icon-16-meetup'></span><span class='social-text'>Meetup</span></a></li>";
+echo "<li><a href='https://plus.google.com/114179804721869477091/about'><span class='social-icon-16 social-icon-16-google-plus'></span><span class='social-text'>Google+</span></a></li>";
+echo "<li><a href='https://twitter.com/raritanphoto'><span class='social-icon-16 social-icon-16-twitter'></span><span class='social-text'>Twitter</span></a></li>";
 echo "</ul>";?></td>
 						<td class="cred-right">Build using the Suffusion theme</td>
 					</tr>
