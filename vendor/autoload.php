3253ebcfd6d28b84d9ac3fde2fd277e57814d028
Peter van der Does <github@avirtualhome.com>
2013-11-20 08:09:03 -0500

    Patchwork/UTF8 is needed

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitf203a82699df0125928dffcd916bd545::getLoader();
+return ComposerAutoloaderInit2f09f0e072cd1ca858bd103f28c54c3d::getLoader();
