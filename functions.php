0730d9c88fdc2d03eb7508dcea136b37dbdf5ab3
Peter van der Does <github@avirtualhome.com>
2014-01-29 23:06:49 -0500

    Version bump 1.5.7-rc.1

--- functions.php
+++ functions.php
@@ -187,7 +187,7 @@ function actionRPS_enqueue_styles()
         $rps_style_version = "15ee5f0";
         wp_enqueue_style('suffusion-theme', get_stylesheet_directory_uri() . '/css/rps-' . $rps_style_version . '.css', array(), 'to_remove');
         // The style version is automatically updated by using git-flow hooks.
-        $rps_js_version = "45a931b";
+        $rps_js_version = "844d407";
         wp_enqueue_script('rps', get_stylesheet_directory_uri() . '/scripts/rps-' . $rps_js_version . '.js', array(), 'to_remove', true);
     }
 
