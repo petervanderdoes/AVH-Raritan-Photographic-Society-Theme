352d3e060eadafd61252394f221b9a721d2f2c86
Peter van der Does <github@avirtualhome.com>
2014-08-09 16:14:13 -0400

    Reveal while loading images.

--- scripts/rps.masonry.js
+++ scripts/rps.masonry.js
@@ -1,13 +1,33 @@
-(function($, window, document) {
+(function ($, window, document) {
     // The $ is now locally scoped
-    $(document).ready(function() {
-	$('.gallery-masonry').masonry({
-	    itemSelector : '.gallery-item-masonry',
-	    columnWidth : '.grid-sizer',
-	    isFitWidth : true
-	}).imagesLoaded(function() {
-	    $('.gallery-masonry').masonry('reloadItems');
-        $('.gallery-masonry').masonry();
-	});
+    $(document).ready(function () {
+        var $container = $('.gallery-masonry').masonry({
+            itemSelector: '.gallery-item-masonry',
+            columnWidth: '.grid-sizer',
+            isFitWidth: true
+        });
+        // reveal initial images
+        $container.masonryImagesReveal($('#images').find('.gallery-item-masonry'));
     });
 }(window.jQuery, window, document));
+
+
+jQuery.fn.masonryImagesReveal = function ($items) {
+    var msnry = this.data('masonry');
+    var itemSelector = msnry.options.itemSelector;
+    // hide by default
+    $items.hide();
+    // append to container
+    this.append($items);
+    $items.imagesLoaded().progress(function (imgLoad, image) {
+        // get item
+        // image is imagesLoaded class, not <img>, <img> is image.img
+        var $item = jQuery(image.img).parents(itemSelector);
+        // un-hide item
+        $item.show();
+        // masonry does its thing
+        msnry.appended($item);
+    });
+
+    return this;
+};
