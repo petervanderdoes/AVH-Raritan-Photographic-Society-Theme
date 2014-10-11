e8af7e6734269aeabd2e4acb08a968c92233175a
Peter van der Does <github@avirtualhome.com>
2014-10-11 12:08:31 -0400

    Version bump 2.0.5-rc.1

--- functions.php
+++ functions.php
@@ -210,10 +210,10 @@ function actionRPS_enqueue_styles()
         wp_enqueue_script('rps', $stylesheet_directory_uri . '/scripts/rps.js', array(), 'to_remove');
     } else {
         // The style version is automatically updated by using git-flow hooks.
-        $rps_style_version = "13b4e6f";
+        $rps_style_version = "5845b67";
         wp_enqueue_style('suffusion-theme', $stylesheet_directory_uri . '/css/rps-' . $rps_style_version . '.css', array(), 'to_remove');
         // The style version is automatically updated by using git-flow hooks.
-        $rps_js_version = "d52d635";
+        $rps_js_version = "5efe7dd";
         wp_enqueue_script('rps', $stylesheet_directory_uri . '/scripts/rps-' . $rps_js_version . '.js', array(), 'to_remove');
     }
 
