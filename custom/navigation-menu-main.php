f88e85731564ea7c3c1ff87c254d351c2e935091
Peter van der Does <github@avirtualhome.com>
2015-03-24 16:01:44 -0400

    Logout url is wrong

--- custom/navigation-menu-main.php
+++ custom/navigation-menu-main.php
@@ -197,11 +197,11 @@ if (trim($ret_str) != '') {
     $ret_str = "<ul class='sf-menu'>\n" . $ret_str . "\n</ul>";
     $login_bar = "<div id='rps-login-bar'>";
     if (is_user_logged_in()) {
-        global $user_ID, $user_identity;
+        global $user_ID, $user_identity, $post;
         $login = get_userdata($user_ID);
         $login_bar .= 'Logged in: <a href="' . admin_url('profile.php') . '">' . $user_identity . "</a>";
-        $redirect = (is_object($post) ? get_permalink($post->ID) : '');
-        $login_bar .= ' | <a href="' . wp_logout_url(apply_filters('the_permalink', get_permalink($post->ID))) . '">Log out</a>';
+        $redirect = (is_object($post) ? apply_filters('the_permalink', get_permalink($post->ID)) : '/');
+        $login_bar .= ' | <a href="' . wp_logout_url($redirect) . '">Log out</a>';
     } else {
         if (isset($_REQUEST["_s2member_seeking"]["_uri"]) && !empty($_GET["_s2member_seeking"]["_uri"])) {
             $redirect = trim(base64_decode(trim(stripslashes((string) $_GET["_s2member_seeking"]["_uri"]))));
