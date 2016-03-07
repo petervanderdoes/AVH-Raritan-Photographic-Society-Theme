621e5bac9ec7446ed27a3c26438f8d17a855427a
Peter van der Does <github@avirtualhome.com>
2016-03-07 16:27:50 -0500

    Skip using WordPress SEO.
    Using it in our own script gives us more flexibility.

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,4 +1,4 @@
-(function ($, window, document) {
+(function ($) {
   // The $ is now locally scoped
   $(function () {
     /**
@@ -8,50 +8,58 @@
       .not('.entry a[href$=".jpg"]')
       .each(function () {
         if (this.hostname !== location.hostname) {
-          
           $(this)
             .attr({
               'class': function (i, val) {
-                val = (val == undefined) ? "" : val;
-                return val + " ui-state-default";
+                val = (val === undefined) ? '' : val + ' ';
+                return val + 'ui-state-default';
               },
-              'target': "_blank",
+              'target': '_blank',
               'title': function (i, val) {
-                val = (val == undefined) ? this.href : val;
-                return val + " (external link, click to open in a new window)";
+                val = (val === undefined) ? this.innerHTML : val;
+                return val + ' (external link, click to open in a new window)';
+              },
+              'onclick': function () {
+                return TrackClick('Outgoing Links', this.href, this.innerHTML);
               }
             });
-          $(this).append("<span class='ui-icon ui-icon-extlink'></span>");
+          $(this).append('<span class=\'ui-icon ui-icon-extlink\'></span>');
           $(this).hover(function () {
-            $(this).addClass("ui-state-hover");
+            $(this).addClass('ui-state-hover');
           }, function () {
-            $(this).removeClass("ui-state-hover");
+            $(this).removeClass('ui-state-hover');
           });
         }
       });
     var rps_IMG_select = '.entry a[href*=".jpg"], .entry area[href*=".jpg"], .entry a[href*=".gif"], .entry area[href*=".gif"], .entry a[href*=".png"], .entry area[href*=".png"]';
     $(rps_IMG_select)
       .each(function () {
-        title = $.url('file', this.href);
         $(this)
           .attr({
-            'onclick': function (i, val) {
-              return "javascript:ga('send','event', 'image', '" + title + "', '" + document.location.pathname + document.location.search + "',  {'nonInteraction': 1});";
+            'onclick': function () {
+              return TrackClick('Images', this.href, this.title);
             }
           });
       });
     var rps_Downloads_select = '.entry a[href*=".pdf"]';
     $(rps_Downloads_select)
       .each(function () {
-        title = $.url(file);
         $(this)
           .attr({
-            'onclick': function (i, val) {
-              return "javascript:ga('send','event', 'download', '" + title + "', '" + document.location.pathname + document.location.search + "',  {'nonInteraction': 1});";
+            'onclick': function () {
+              return TrackClick('Downloads', this.href, this.innerHTML);
             }
           });
       });
   });
-}(window.jQuery, window, document));
+}(window.jQuery));
 // The global jQuery object is passed as a parameter
-
+/**
+ * @return {string}
+ */
+function TrackClick
+(eventCategory, eventAction, eventLabel) {
+  var n = eventAction.indexOf('?');
+  eventAction = eventAction.substring(0, n !== -1 ? n : eventAction.length);
+  return '__gaTracker(\'send\',\'event\', \'' + eventCategory + '\', \'' + decodeURIComponent(eventAction) + '\', \'' + decodeURIComponent(eventLabel) + '\';, {\'nonInteraction\': 1}';
+}
