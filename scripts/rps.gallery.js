7567eb358270fdf771d8e32d150cc4c213095924
Peter van der Does <github@avirtualhome.com>
2016-03-09 11:16:38 -0500

    Update to coding standard

--- scripts/rps.gallery.js
+++ scripts/rps.gallery.js
@@ -1,24 +1,29 @@
 (function ($) {
   var media = wp.media;
+
   // Wrap the render() function to append controls.
   media.view.Settings.Gallery = media.view.Settings.Gallery.extend({
     render: function () {
       var $el = this.$el;
       media.view.Settings.prototype.render.apply(this, arguments);
+
       // Append the type template and update the settings.
       $el.append(media.template('rps-gallery-settings'));
-      media.gallery.defaults.type = 'default'; // lil hack that lets media know there's a type attribute.
+
+      // lil hack that lets media know there's a type attribute.
+      media.gallery.defaults.type = 'default';
       this.update.apply(this, ['layout']);
+
       // Hide the Columns setting for all types except Default
       $el.find('select[name=layout]').on('change', function () {
         var columnSetting = $el.find('select[name=columns]').closest('label.setting');
-        if ('masonry' === $(this).val()) {
+        if ($(this).val() === 'masonry') {
           columnSetting.hide();
         } else {
           columnSetting.show();
         }
       }).change();
       return this;
-    }
+    },
   });
 })(jQuery);
--- scripts/rps.js
+++ scripts/rps.js
@@ -4,81 +4,107 @@
     /**
      * Setup external links
      */
-    $('.entry a[href^="http://"] , #comments a[href^="http://"] , .entry a[href^="https://"] , #comments a[href^="https://"]')
+    var Links = '.entry a[href^="http://"]' +
+      ', #comments a[href^="http://"]' +
+      ', .entry a[href^="https://"]' +
+      ', #comments a[href^="https://"]';
+    $(Links)
       .not('.entry a[href$=".jpg"]')
       .each(function () {
         if (this.hostname !== location.hostname) {
           $(this)
             .attr({
-              'class': function (i, val) {
+              class: function (i, val) {
                 val = (val === undefined) ? '' : val + ' ';
                 return val + 'ui-state-default';
               },
-              'rel': 'external nofollow',
-              'target': '_blank',
-              'title': function (i, val) {
+
+              rel: 'external nofollow',
+              target: '_blank',
+              title: function (i, val) {
                 val = !val ? this.innerHTML : val;
                 return val + ' (external link, click to open in a new window)';
               },
-              'onclick': function () {
+
+              onclick: function () {
                 var eventAction = ExtractDomain(this.href);
                 return TrackClick('Outgoing Links', eventAction, this.innerHTML);
-              }
+              },
             });
           $(this).append('<span class=\'ui-icon ui-icon-extlink\'></span>');
           $(this).hover(function () {
             $(this).addClass('ui-state-hover');
           }, function () {
+
             $(this).removeClass('ui-state-hover');
           });
         }
       });
-    var rps_IMG_select = '.rps-showcases a[href*=".jpg"], .entry a[href*=".jpg"], .entry area[href*=".jpg"], .entry a[href*=".gif"], .entry area[href*=".gif"], .entry a[href*=".png"], .entry area[href*=".png"]';
-    $(rps_IMG_select)
+
+    var RpsImages = '.rps-showcases a[href*=".jpg"]' +
+      ', .entry a[href*=".jpg"]' +
+      ', .entry area[href*=".jpg"]' +
+      ', .entry a[href*=".gif"]' +
+      ', .entry area[href*=".gif"]' +
+      ', .entry a[href*=".png"]' +
+      ', .entry area[href*=".png"]';
+    $(RpsImages)
       .each(function () {
         $(this)
           .attr({
-            'onclick': function () {
+            onclick: function () {
               return TrackClick('Images', window.location.pathname, this.title);
             },
-            'title': function (i, val) {
+
+            title: function (i, val) {
               if (!val) {
                 var image = $(this).find('img').first();
                 val = image[0].alt;
               }
+
               return val;
-            }
+            },
           });
       });
-    var rps_Downloads_select = '.entry a[href*=".pdf"]';
-    $(rps_Downloads_select)
+
+    var RpsDownloads = '.entry a[href*=".pdf"]';
+    $(RpsDownloads)
       .each(function () {
         $(this)
           .attr({
-            'onclick': function () {
+            onclick: function () {
               return TrackClick('Downloads', window.location.pathname, this.innerHTML);
-            }
+            },
           });
       });
   });
 }(window.jQuery, window));
+
 // The global jQuery object is passed as a parameter
 /**
  * @return {string}
  */
 function TrackClick
 (eventCategory, eventAction, eventLabel) {
-  return '__gaTracker(\'send\',\'event\', \'' + eventCategory + '\', \'' + decodeURIComponent(eventAction) + '\', \'' + decodeURIComponent(eventLabel) + '\';, {\'nonInteraction\': 1}';
+  return '__gaTracker(\'send\',\'event\', \'' +
+    eventCategory +
+    '\', \'' +
+    decodeURIComponent(eventAction) +
+    '\', \'' +
+    decodeURIComponent(eventLabel) +
+    '\';, {\'nonInteraction\': 1}';
 }
+
 function ExtractDomain(url) {
   var domain;
+
   //find & remove protocol (http, ftp, etc.) and get domain
   if (url.indexOf('://') > -1) {
     domain = url.split('/')[2];
-  }
-  else {
+  } else {
     domain = url.split('/')[0];
   }
+
   //find & remove port number
   domain = domain.split(':')[0];
   return domain;
